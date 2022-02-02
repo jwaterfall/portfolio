31c3c8640f947912d5087f918569b9abecd5e5bd
jwaterfall <jack.a.waterfall@gmail.com>
2022-02-02 01:14:55 +0000

    Removed active section detection due to side effect of breaking navbar links

--- components/elements/Section/index.tsx
+++ components/elements/Section/index.tsx
@@ -1,6 +1,4 @@
-import { useRouter } from 'next/router';
-import { FC, useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { FC } from 'react';
 import { Container } from './styles';
 
 export type SectionVariant = 'primary' | 'secondary';
@@ -10,19 +8,10 @@ export interface SectionProps {
   variant?: SectionVariant;
 }
 
-const Section: FC<SectionProps> = ({ hash, variant = 'primary', children }) => {
-  const { ref, inView } = useInView({ threshold: 0.5 });
-  const { push } = useRouter();
-
-  useEffect(() => {
-    if (inView) push(`/#${hash}`);
-  }, [inView, hash, push]);
-
-  return (
-    <Container id={hash} variant={variant} ref={ref}>
-      {children}
-    </Container>
-  );
-};
+const Section: FC<SectionProps> = ({ hash, variant = 'primary', children }) => (
+  <Container id={hash} variant={variant}>
+    {children}
+  </Container>
+);
 
 export default Section;
