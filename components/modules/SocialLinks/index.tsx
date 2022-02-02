cc8a26c8e6b0382fb09004b0526c451ecc843eb9
jwaterfall <jack.a.waterfall@gmail.com>
2022-02-02 01:09:02 +0000

    Updated social links

--- components/modules/SocialLinks/index.tsx
+++ components/modules/SocialLinks/index.tsx
@@ -5,18 +5,21 @@ import { Container, SocialLink } from './styles';
 
 const SocialLinks: FC = () => (
   <Container>
-    <SocialLink>
+    <SocialLink href="https://github.com/jwaterfall/portfolio" target="_blank">
       <FaGithub />
     </SocialLink>
-    <SocialLink>
+    <SocialLink
+      href="https://www.linkedin.com/in/jack-waterfall-7384571a6/"
+      target="_blank"
+    >
       <FaLinkedinIn />
     </SocialLink>
-    <SocialLink>
+    {/* <SocialLink>
       <FaTwitter />
     </SocialLink>
     <SocialLink>
       <FaInstagram />
-    </SocialLink>
+    </SocialLink> */}
   </Container>
 );
 
